a0870cefdae4a85b8fb2ab3ca8dc194c843a1bf0
NichoUlloa <n.ulloa06@ufromail.cl>
2023-11-20 15:40:42 -0300

    Se crea la clase abstract Persona para heredar Bibliotecario y Usuario, tambien se modifica la clase prestamo y GestorPDF. Tambien se agrega enumeracion de tipos de libros.

--- src/main/java/modelo/Libro.java
+++ src/main/java/modelo/Libro.java
@@ -6,6 +6,7 @@ public class Libro {
     private String Editorial;
     private String ISBN;
 
+
     public Libro(String NombreLibro, String Autor, String Editorial, String ISBN) {
         this.NombreLibro = NombreLibro;
         this.Autor = Autor;
